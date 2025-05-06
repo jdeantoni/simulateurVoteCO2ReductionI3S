d69ae70fbd3c358086ce98d49ae4107b981b6032
Julien Deantoni <julien.deantoni@univ-cotedazur.fr>
2025-05-06 14:23:00 +0200

    verified pourcentage

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def index():
     rows = [
     {"title": "Déplacement domicile travail bas carbone"+createTooltipContent("vélo, marche, transport en commun, co-voiturage, télétravail"), "parts": [
         {"text": "0 jour par semaine", "weight": 0},
-        {"text": "1 jour par semaine", "weight": 1*5},
-        {"text": "2 jour par semaine", "weight": 2*5, "checked":True},
-        {"text": "3 jour par semaine", "weight": 3*5},
+        {"text": "1 jour par semaine", "weight": 1*4.3},
+        {"text": "2 jour par semaine", "weight": 2*4.3, "checked":True},
+        {"text": "3 jour par semaine", "weight": 3*4.3},
     ]},
     {"title": "Vie au laboratoire:<br> Restauration événementielle", "parts": [
         {"text": "repas bas carbone, 50% végétarien", "weight": 1},
@@ -31,21 +31,21 @@ def index():
         {"text": "repas bas carbone, 100% végétarien", "weight": 2},
     ]},
      {"title": "Vie au laboratoire:<br> Déjeuner"+createTooltipContent("Impact moyen de différents types de repas (source ademe)" ,"impact_co2_dejeuner.png"), "parts": [
-        {"text": "1 déjeuner par semaine < 1KCo2e", "weight": 1*2.8},
-        {"text": "2 déjeuner par semaine < 1KCo2e", "weight": 2*2.8},
-        {"text": "3 déjeuner par semaine < 1KCo2e", "weight": 3*2.8, "checked":True},
-        {"text": "4 déjeuner par semaine < 1KCo2e", "weight": 4*2.8},
+        {"text": "1 déjeuner par semaine < 1KCo2e", "weight": 1*1.76},
+        {"text": "2 déjeuner par semaine < 1KCo2e", "weight": 2*1.76},
+        {"text": "3 déjeuner par semaine < 1KCo2e", "weight": 3*1.76, "checked":True},
+        {"text": "4 déjeuner par semaine < 1KCo2e", "weight": 4*1.76},
     ]},
     {"title": "Missions: <br>report avion -> train", "parts": [
         {"text": "report si < 5 heures", "weight": 0.3},
-        {"text": "report si < 8 heures", "weight": 9*0.4, "checked":True},
+        {"text": "report si < 8 heures", "weight": 9*0.323, "checked":True},
     ]},
     {"title": "Missions: <br>quota", "parts": [
         {"text": "Pas de quota", "weight": 0},
-        {"text": "9 tonnes sur 3 ans glissants", "weight": 7.5*0.4},
-        {"text": "6 tonnes sur 3 ans glissants", "weight": 12*0.4, "checked":True},
-        {"text": "4.5 tonnes sur 3 ans glissants", "weight": 18*0.4},
-        {"text": "3 tonnes sur 3 ans glissants", "weight": 29*0.4},
+        {"text": "9 tonnes sur 3 ans glissants", "weight": 7.5*0.323},
+        {"text": "6 tonnes sur 3 ans glissants", "weight": 12*0.323, "checked":True},
+        {"text": "4.5 tonnes sur 3 ans glissants", "weight": 18*0.323},
+        {"text": "3 tonnes sur 3 ans glissants", "weight": 29*0.323},
     ]},
 ]
 
